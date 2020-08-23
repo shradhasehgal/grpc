913aa886a1835242bc89962db791906395154c23
Shradha <shradha.sgl@gmail.com>
2020-08-23 16:41:59 +0530

    Cleaned code

--- client.cc
+++ client.cc
@@ -1,62 +1,73 @@
+#include <grpcpp/grpcpp.h>
+
 #include <string>
 
-#include <grpcpp/grpcpp.h>
 #include "stringreverse.grpc.pb.h"
 
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
 
-using stringreverse::StringReverse;
-using stringreverse::StringRequest;
 using stringreverse::StringReply;
+using stringreverse::StringRequest;
+using stringreverse::StringReverse;
 
 class StringReverseClient {
-    public:
-        StringReverseClient(std::shared_ptr<Channel> channel) : stub_(StringReverse::NewStub(channel)) {}
+ public:
+  StringReverseClient(std::shared_ptr<Channel> channel)
+      : stub_(StringReverse::NewStub(channel)) {}
 
-    std::string sendRequest(std::string a) {
-        StringRequest request;
+  // Assembles client payload, sends it to the server, and returns its response
+  std::string sendRequest(std::string a) {
+    // Data to be sent to server
+    StringRequest request;
+    request.set_original(a);
 
-        request.set_a(a);
+    // Container for server response
+    StringReply reply;
 
-        StringReply reply;
+    // Context can be used to send meta data to server or modify RPC behaviour
+    ClientContext context;
 
-        ClientContext context;
+    // Actual Remote Procedure Call
+    Status status = stub_->sendRequest(&context, request, &reply);
 
-        Status status = stub_->sendRequest(&context, request, &reply);
-
-        if(status.ok()){
-            return reply.message();
-        } else {
-            std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-            return "Error";
-        }
+    // Returns results based on RPC status
+    if (status.ok()) {
+      return reply.reversed();
+    } else {
+      std::cout << status.error_code() << ": " << status.error_message()
+                << std::endl;
+      return "RPC Failed";
     }
+  }
 
-    private:
-        std::unique_ptr<StringReverse::Stub> stub_;
+ private:
+  std::unique_ptr<StringReverse::Stub> stub_;
 };
 
 void RunClient() {
-    std::string target_address("0.0.0.0:50051");
-    StringReverseClient client(
-        grpc::CreateChannel(
-            target_address, 
-            grpc::InsecureChannelCredentials()
-        )
-    );
+  std::string target_address("0.0.0.0:50051");
+  // Instantiates the client
+  StringReverseClient client(
+      // Channel from which RPCs are made - endpoint is the target_address
+      grpc::CreateChannel(target_address,
+                          // Indicate when channel is not authenticated
+                          grpc::InsecureChannelCredentials()));
 
-    std::string response;
-    std::string a ="shradha";
+  std::string response;
+  std::string a = "grpc is cool!";
 
-    response = client.sendRequest(a);
-    std::cout << "Original string: "<< a << std::endl;
-    std::cout << "Reversed string: "<< response << std::endl;
+  // RPC is created and response is stored
+  response = client.sendRequest(a);
+
+  // Prints results
+  std::cout << "Original string: " << a << std::endl;
+  std::cout << "Reversed string: " << response << std::endl;
 }
 
-int main(int argc, char* argv[]){
-    RunClient();
+int main(int argc, char* argv[]) {
+  RunClient();
 
-    return 0;
+  return 0;
 }
--- server.cc
+++ server.cc
@@ -1,52 +1,54 @@
+#include <bits/stdc++.h>
+#include <grpcpp/grpcpp.h>
+
 #include <string>
 
-#include <bits/stdc++.h> 
-#include <grpcpp/grpcpp.h>
 #include "stringreverse.grpc.pb.h"
 
 using grpc::Server;
 using grpc::ServerBuilder;
 using grpc::ServerContext;
 using grpc::Status;
-using stringreverse::StringReverse;
-using stringreverse::StringRequest;
+
 using stringreverse::StringReply;
+using stringreverse::StringRequest;
+using stringreverse::StringReverse;
 
+// Server Implementation
 class ReverseServiceImplementation final : public StringReverse::Service {
-    Status sendRequest(
-        ServerContext* context, 
-        const StringRequest* request, 
-        StringReply* reply
-    ) override {
-        std::string a = request->a();
+  Status sendRequest(ServerContext* context, const StringRequest* request,
+                     StringReply* reply) override {
+    // Obtains the original string from the request
+    std::string a = request->original();
 
-        int n = a.length(); 
-        for (int i = 0; i < n / 2; i++) 
-            std::swap(a[i], a[n - i - 1]); 
+    // String reversal
+    int n = a.length();
+    for (int i = 0; i < n / 2; i++) std::swap(a[i], a[n - i - 1]);
 
-        reply->set_message(a);
-
-        return Status::OK;
-    } 
+    reply->set_reversed(a);
+    return Status::OK;
+  }
 };
 
 void RunServer() {
-    std::string server_address("0.0.0.0:50051");
-    ReverseServiceImplementation service;
+  std::string server_address("0.0.0.0:50051");
+  ReverseServiceImplementation service;
 
-    ServerBuilder builder;
-    // Listen on the given address without any authentication mechanism.
-    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-    // Register "service" as the instance through which we'll communicate with clients
-    builder.RegisterService(&service);
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which
+  // communication with client takes place
+  builder.RegisterService(&service);
 
-    std::unique_ptr<Server> server(builder.BuildAndStart());
-    std::cout << "Server listening on port: " << server_address << std::endl;
+  // Assembling the server
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on port: " << server_address << std::endl;
 
-    server->Wait();
+  server->Wait();
 }
 
 int main(int argc, char** argv) {
-    RunServer();
-    return 0;
+  RunServer();
+  return 0;
 }
